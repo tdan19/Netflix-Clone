e5ef7c7aece40f799e519ef60f0a594019d1a1b6
Daniel Damtew <167955315+tdan19@users.noreply.github.com>
2025-04-08 11:44:00 +0300

    intial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
+import Row from './Row';
+import requests from './requests';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+   <Row title=" NETFLIX ORGINALS" fetchUrl={requests.fetchNetflixOriginals} 
+   isLargeRow
+   />  
+   <Row title="Trending Now" fetchUrl={requests.fetchTrending} /> 
+   <Row title="Top Rated" fetchUrl={requests.fetchTopRatedMovies}/>  
+   <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} /> 
+   <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies}/>  
+   <Row title="Horror Movie" fetchUrl={requests.fetchHorrorMovies} /> 
+   <Row title=" Romance Movie" fetchUrl={requests.fetchRomanceMovies}/>  
+   <Row title="Documentaries Movie" fetchUrl={requests.fetchDocumentaries} />  
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
